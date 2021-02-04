e73ab5d6b16d4857f49930f3b85fc321e6877f82
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-02-04 20:23:15 +0000

    modelling unlocking logic

--- erc721-locker/contracts/ERC721Locker.sol
+++ erc721-locker/contracts/ERC721Locker.sol
@@ -1,11 +1,16 @@
 pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "rainbow-bridge/contracts/eth/nearbridge/contracts/Borsh.sol";
+import "rainbow-bridge/contracts/eth/nearprover/contracts/ProofDecoder.sol";
 
 import { IERC721Locker } from "./interfaces/IERC721Locker.sol";
 import { INearProver, Locker } from "./Locker.sol";
+import { StringToUint256 } from "./StringToUint256.sol";
 
 contract ERC721Locker is IERC721Locker, Locker {
+    using StringToUint256 for string;
+
     event LockedForNativeNear (
         address indexed token,
         address indexed sender,
@@ -20,6 +25,19 @@ contract ERC721Locker is IERC721Locker, Locker {
         address nearEvmAddress,
         uint256 migrationFee
     );
+
+    event Unlocked (
+        address token,
+        uint256 tokenId,
+        address recipient
+    );
+
+    // Function output from burning non-fungible token on Near side.
+    struct BurnResult {
+        string tokenId; // comes back as string as Near side cannot handle uint256
+        address token;
+        address recipient;
+    }
 
     constructor(bytes memory _nearTokenFactory, INearProver _nearProver) public {
         nearTokenFactory_ = _nearTokenFactory;
@@ -37,6 +55,26 @@ contract ERC721Locker is IERC721Locker, Locker {
     }
 
     function unlockToken(bytes calldata _proofData, uint64 _proofBlockHeader) external override {
+        ProofDecoder.ExecutionStatus memory status = _parseProof(_proofData, _proofBlockHeader);
+        BurnResult memory result = _decodeBurnResult(status.successValue);
 
+        uint256 tokenId = result.tokenId.toUint256();
+
+        IERC721(result.token).safeTransferFrom(address(this), result.recipient, tokenId);
+
+        emit Unlocked(result.token, tokenId, result.recipient);
+    }
+
+    function _decodeBurnResult(bytes memory data) internal pure returns(BurnResult memory result) {
+        Borsh.Data memory borshData = Borsh.from(data);
+
+        uint8 flag = borshData.decodeU8();
+        require(flag == 0, "ERR_NOT_WITHDRAW_RESULT");
+
+        return BurnResult({
+            tokenId: string(borshData.decodeBytes()),
+            token: address(uint160(borshData.decodeBytes20())),
+            recipient: address(uint160(borshData.decodeBytes20()))
+        });
     }
 }
---
+++ erc721-locker/contracts/StringToUint256.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.6.12;
+
+library StringToUint256 {
+    function toUint256(string memory s) internal pure returns (uint256 result) {
+        bytes memory b = bytes(s);
+        uint i;
+        result = 0;
+        for (i = 0; i < b.length; i++) {
+            uint c = uint(uint8(b[i]));
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + (c - 48);
+            }
+        }
+    }
+}
