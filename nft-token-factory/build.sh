bd3da595c20926995cf5b9ce06c212d639b3958c
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-05-22 11:48:48 +0100

    better build scripts

--- nft-token-factory/build.sh
+++ nft-token-factory/build.sh
@@ -3,4 +3,4 @@ set -e
 cd "`dirname $0`"
 source ./flags.sh
 cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/near_bridge.wasm ./res/
+cp target/wasm32-unknown-unknown/release/nft_token_factory.wasm ../res/
