16e630bf320855e563f9702168dfb1961a656ade
idirall22 <idirall22@gmail.com>
2021-12-06 08:47:47 +0100

    convert erc721 repo to typescript

--- ERC721Locker/hardhat.config.ts
+++ ERC721Locker/hardhat.config.ts
@@ -4,7 +4,9 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "@openzeppelin/hardhat-upgrades";
 import "hardhat-gas-reporter";
+import "hardhat-contract-sizer";
 import "solidity-coverage";
 
 dotenv.config();
@@ -50,6 +52,12 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true
+  }
 };
 
 export default config;
