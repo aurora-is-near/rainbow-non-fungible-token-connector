c56d1ef25919761f4629c629c1bef6e7ecb0a023
idirall22 <idirall22@gmail.com>
2021-12-06 10:37:02 +0100

    added a script to export abi and bytecode

--- ERC721Locker/hardhat.config.ts
+++ ERC721Locker/hardhat.config.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
