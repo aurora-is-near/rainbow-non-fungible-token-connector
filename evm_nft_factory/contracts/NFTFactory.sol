2e10efc2f947660465cb27faa24b7e02939e50c1
idirall22 <idirall22@gmail.com>
2021-12-11 09:05:38 +0100

    implemented the proof check before mint the token

--- evm_nft_factory/contracts/NFTFactory.sol
+++ evm_nft_factory/contracts/NFTFactory.sol
@@ -2,24 +2,149 @@
 pragma solidity 0.8.7;
 
 import "hardhat/console.sol";
+import "rainbow-bridge/contracts/eth/nearprover/contracts/ProofDecoder.sol";
+import "rainbow-bridge/contracts/eth/nearprover/contracts/INearProver.sol";
 
 contract NFTFactory {
+    using Borsh for Borsh.Data;
+    using ProofDecoder for Borsh.Data;
+
+    event ConsumedProof(bytes32 indexed _receiptId);
+
     /// @notice this mapping stores the near contract name with the evm contract copy address.
     /// ie: NFT => 0x123456
     mapping(string => address) public bridgedNFTs;
 
     /// @notice the near prover address.
-    address public prover;
+    INearProver public nearProver;
 
-    constructor(address _prover) {
-        prover = _prover;
+    /// @notice the near prover address.
+    bytes public nearLocker;
+
+    /// @notice the near prover address.
+    uint64 minBlockAcceptanceHeight;
+
+    // OutcomeReciptId -> Used
+    mapping(bytes32 => bool) public usedEvents;
+
+    constructor(
+        INearProver _nearProver,
+        bytes memory _nearLocker,
+        uint64 _minBlockAcceptanceHeight
+    ) {
+        nearProver = _nearProver;
+        minBlockAcceptanceHeight = _minBlockAcceptanceHeight;
+        nearLocker = _nearLocker;
     }
 
     /// @notice This function allows to finalise the bridge process by calling the
     /// evm contract and mint the new token.
     /// @dev ***DON'T DO THIS ONE FOR NOW***.
     /// @param _proofData near proof.
-    function finaliseNearToEthTransfer(bytes calldata _proofData) external {}
+    function finaliseNearToEthTransfer(
+        bytes calldata _proofData,
+        uint64 _proofBlockHeader
+    ) external {
+        ProofDecoder.ExecutionStatus memory status = _parseAndConsumeProof(
+            _proofData,
+            _proofBlockHeader
+        );
+
+        Borsh.Data memory borshDataFromProof = Borsh.from(status.successValue);
+
+        uint8 flag = Borsh.decodeU8(borshDataFromProof);
+        require(flag == 0, "ERR_NOT_LOCK_RESULT");
+
+        address recipient = address(
+            uint160(Borsh.decodeBytes20(borshDataFromProof))
+        );
+
+        string memory accountID = string(Borsh.decodeBytes(borshDataFromProof));
+
+        string memory tokenIdAsString = string(
+            Borsh.decodeBytes(borshDataFromProof)
+        );
+        uint256 tokenId = stringToUint(tokenIdAsString);
+
+        //TODO: check if the contract was already deployed add added to "bridgedNFTs" mapping
+        // accountID: the near contract name "NFT"
+
+
+        // TODO: call the spécific Bridged contract and mint new Token using
+        // recipient: the eth account that will receive the token
+        // accountID: the near contract name "NFT"
+        // tokenID: uint256
+    }
+
+    function _parseAndConsumeProof(
+        bytes memory proofData,
+        uint64 proofBlockHeight
+    ) internal returns (ProofDecoder.ExecutionStatus memory result) {
+        require(
+            nearProver.proveOutcome(proofData, proofBlockHeight),
+            "Proof should be valid"
+        );
+
+        // Unpack the proof and extract the execution outcome.
+        Borsh.Data memory borshData = Borsh.from(proofData);
+        ProofDecoder.FullOutcomeProof memory fullOutcomeProof = borshData
+            .decodeFullOutcomeProof();
+
+        require(
+            fullOutcomeProof.block_header_lite.inner_lite.height >=
+                minBlockAcceptanceHeight,
+            "Proof is from the ancient block"
+        );
+
+        bytes32 receiptId = fullOutcomeProof
+            .outcome_proof
+            .outcome_with_id
+            .outcome
+            .receipt_ids[0];
+        require(!usedEvents[receiptId], "The lock event cannot be reused");
+        usedEvents[receiptId] = true;
+
+        require(
+            keccak256(
+                fullOutcomeProof
+                    .outcome_proof
+                    .outcome_with_id
+                    .outcome
+                    .executor_id
+            ) == keccak256(nearLocker),
+            "Can only mint tokens from the linked mintable not fungible token on Near blockchain."
+        );
+
+        result = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.status;
+        require(
+            !result.failed,
+            "Cannot use failed execution outcome for minting the tokens."
+        );
+        require(
+            !result.unknown,
+            "Cannot use unknown execution outcome for minting the tokens."
+        );
+
+        emit ConsumedProof(receiptId);
+    }
+
+    function stringToUint(string memory s) public pure returns (uint256) {
+        bytes memory b = bytes(s);
+        uint256 result = 0;
+        uint256 oldResult = 0;
+        for (uint256 i = 0; i < b.length; i++) {
+            if (uint8(b[i]) >= 48 && uint8(b[i]) <= 57) {
+                oldResult = result;
+                result = result * 10 + (uint256(uint8(b[i])) - 48);
+                if (oldResult > result) {
+                    revert("Invalid number");
+                }
+            } else {
+                revert("Invalid number");
+            }
+        }
+        return result;
+    }
 
     /// @notice Deploy a new Bridged (ERC721) contract.
     /// @dev before deploying the contract we have to check if the contract was already
---
+++ evm_nft_factory/test/NFTFactory.test.ts
@@ -0,0 +1,89 @@
+import hardhat, { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {
+  NFTFactory,
+  NFTFactory__factory,
+  NearProverMock,
+  NearProverMock__factory,
+} from "../typechain";
+// import { Signer, Contract, BigNumber } from "ethers";
+// import chai, { expect } from "chai";
+// import { solidity } from "ethereum-waffle";
+// import { Artifact } from "hardhat/types";
+const {
+  borshifyOutcomeProof,
+} = require("rainbow-bridge-lib/rainbow/borshify-proof.js");
+const { serialize } = require("rainbow-bridge-lib/rainbow/borsh.js");
+
+let NFTFactoryContract: NFTFactory;
+let NearMockContract: NearProverMock;
+let signer: SignerWithAddress;
+
+const SCHEMA = {
+  Locked: {
+    kind: "struct",
+    fields: [
+      ["flag", "u8"],
+      ["recipient", [20]],
+      ["tokenAccountIdStringLength", [4]],
+      ["tokenAccountId", [3]],
+      ["tokenIdStringLength", [4]],
+      ["tokenId", [2]],
+    ],
+  },
+};
+
+describe("NodeOperator", function () {
+  beforeEach(async function () {
+    const accounts = await ethers.getSigners();
+    signer = accounts[0];
+
+    const nearMockFactory: NearProverMock__factory =
+      (await hardhat.ethers.getContractFactory(
+        "NearProverMock"
+      )) as NearProverMock__factory;
+
+    NearMockContract = (await nearMockFactory.deploy()) as NearProverMock;
+
+    const nftFactoryArtifact: NFTFactory__factory =
+      (await hardhat.ethers.getContractFactory(
+        "NFTFactory"
+      )) as NFTFactory__factory;
+
+    NFTFactoryContract = (await nftFactoryArtifact.deploy(
+      NearMockContract.address,
+      Buffer.from("nearnonfuntoken", "utf-8"),
+      0
+    )) as NFTFactory;
+  });
+
+  it("finaliseNearToEthTransfer", async function () {
+    console.log(hardhat.ethers.utils.arrayify(Buffer.from("NFT", "utf-8")));
+    proof.outcome_proof.outcome.status.SuccessValue = serialize(
+      SCHEMA,
+      "Locked",
+      {
+        flag: 0,
+        recipient: hardhat.ethers.utils.arrayify(signer.address),
+        tokenAccountIdStringLength: int32ToBytes(3),
+        tokenAccountId: Buffer.from("NFT", "utf-8"),
+        tokenIdStringLength: int32ToBytes(2),
+        tokenId: Buffer.from("22", "utf-8"),
+      }
+    ).toString("base64");
+
+    await NFTFactoryContract.finaliseNearToEthTransfer(
+      borshifyOutcomeProof(proof),
+      10
+    );
+  });
+
+  const proof = require("./proof1.json");
+});
+
+function int32ToBytes(num: number) {
+  const arr = new ArrayBuffer(4); // an Int32 takes 4 bytes
+  const view = new DataView(arr);
+  view.setUint32(0, num, true); // byteOffset = 0; litteEndian = true as Borsh library is little endian
+  return new Uint8Array(arr);
+}
